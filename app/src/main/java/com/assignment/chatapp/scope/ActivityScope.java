609ae4b2c1dd7d56e756f96ff83daaa8b1dc967d
ankitsrivastava <ankit.srivastava@rubique.com>
2019-06-03 12:52:07 +0530

    version_3 changes

--- app/src/main/java/com/assignment/chatapp/scope/ActivityScope.java
+++ app/src/main/java/com/assignment/chatapp/scope/ActivityScope.java
@@ -5,9 +5,7 @@ import java.lang.annotation.RetentionPolicy;
 
 import javax.inject.Scope;
 
-/**
- * Created by ravindra on 08,June,2018
- */
+
 @Scope
 @Retention(RetentionPolicy.RUNTIME)
 public @interface ActivityScope {
--- app/src/main/java/com/assignment/chatapp/scope/UserScope.java
+++ app/src/main/java/com/assignment/chatapp/scope/UserScope.java
@@ -5,9 +5,7 @@ import java.lang.annotation.RetentionPolicy;
 
 import javax.inject.Scope;
 
-/**
- * Created by ravindra on 14,May,2018
- */
+
 @Scope
 @Retention(RetentionPolicy.RUNTIME)
 public @interface UserScope {
